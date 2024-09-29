394b331bdd717bdd0e12d76ac6cbbd65326b4a5d
anonymous-lad2 <anonymouslad540@gmail.com>
2024-09-29 10:53:34 +0530

    done all the setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Cart } from './pages/Cart';
+import { Navbar } from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div>
+
+      <div className='bg-slate-900'>
+        <Navbar />
+      </div>
+    
+
+      <Routes>
+        <Route path='/' element= {<Home />} />
+        <Route path='/cart' element = {<Cart />} />
+      </Routes>
+
     </div>
   );
 }
